5484e26ce6f0152cbeed4aa02abc58bfc707adfe
gauravkakad1 <gauravkakad2468@gmail.com>
2024-02-28 13:28:28 +0530

    added focus function and zoom

--- lib/camerapgae.dart
+++ lib/camerapgae.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:path/path.dart' as path;
+import 'package:path/path.dart' as path;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraPage extends StatefulWidget {
   final List<CameraDescription>? cameras;
@@ -21,10 +22,11 @@ class _CameraPageState extends State<CameraPage> {
   int description = 0;
   bool isPhotoMode = true;
   bool frontCamera = true;
-
+  double _zoomValue = 1.0;
   bool? isFlashOn = false;
   bool flashClicked = false;
   FlashMode flashMode = FlashMode.auto;
+  Offset? _focusPoint;
   Icon flashIcon = Icon(
     Icons.flash_auto,
     color: Colors.amber,
@@ -41,13 +43,8 @@ class _CameraPageState extends State<CameraPage> {
     try {
       pictureFile = await controller.takePicture();
       if (pictureFile == null) return;
-      // final Directory appDir = await getApplicationSupportDirectory();
-      // final String capturedPath =
-      //     path.join(appDir.path, '${DateTime.now()}.jpg');
 
       await GallerySaver.saveImage(pictureFile!.path);
-      // final File savedFile = File(pictureFile!.path);
-      // await savedFile.rename(capturedPath);
 
       changeState();
     } catch (e) {
@@ -76,6 +73,33 @@ class _CameraPageState extends State<CameraPage> {
     });
   }
 
+  void zoomCamera(double zoom) {
+    _zoomValue = zoom;
+    controller.setZoomLevel(zoom);
+    changeState();
+  }
+
+  Future<void> setCameraFocusPoint(Offset relativeTapPosition) async {
+    print(
+        relativeTapPosition.dx.toString() + relativeTapPosition.dy.toString());
+    if (!controller.value.isInitialized && controller == null) return;
+    try {
+      final double dx = relativeTapPosition.dx.clamp(0.0, 0.1);
+      final double dy = relativeTapPosition.dy.clamp(0.0, 0.1);
+      _focusPoint = Offset(dx, dy);
+
+      await controller.setFocusPoint(_focusPoint!);
+      await controller.setFocusMode(FocusMode.auto);
+      print(dx.toString() + "  " + dy.toString());
+      changeState();
+      await Future.delayed(Duration(seconds: 2));
+      _focusPoint = null;
+      changeState();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   void toggleCamera() {
     description = description == 0 ? 1 : 0;
     startCamera(description);
@@ -102,153 +126,199 @@ class _CameraPageState extends State<CameraPage> {
         ),
       );
     }
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Camera'),
-      //   centerTitle: true,
-      //   automaticallyImplyLeading: false,
-      //   elevation: 20,
-      // ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned.fill(
-                child: AspectRatio(
-                    aspectRatio: controller.value.aspectRatio,
-                    child: CameraPreview(controller))),
-            Positioned(
-              top: 30,
-              left: 20,
-              right: 20,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                      onTap: () async {
-                        flashClicked = !flashClicked;
-                        setState(() {});
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Camera'),
+        //   centerTitle: true,
+        //   automaticallyImplyLeading: false,
+        //   elevation: 20,
+        // ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Stack(
+              children: [
+                Positioned.fill(
+                    child: AspectRatio(
+                        aspectRatio: controller.value.aspectRatio,
+                        child: GestureDetector(
+                            onTapDown: (details) {
+                              final Offset tapPosition = details.localPosition;
+
+                              final Offset relativeTapPosition = Offset(
+                                  tapPosition.dx / constraints.maxWidth,
+                                  tapPosition.dy / constraints.maxHeight);
+                              setCameraFocusPoint(relativeTapPosition);
+                            },
+                            child: CameraPreview(controller)))),
+                Positioned(
+                    bottom: 200,
+                    right: 100,
+                    child: Slider(
+                      activeColor: Colors.amber,
+                      inactiveColor: Colors.white,
+                      thumbColor: Colors.white,
+                      value: _zoomValue,
+                      min: 1.0,
+                      max: 4.0,
+                      onChanged: (value) {
+                        zoomCamera(value);
+                        // changeState();
                       },
-                      child: flashClicked
-                          ? flashRow()
-                          : SizedBox(height: 50, width: 50, child: flashIcon)),
-                  Visibility(
-                    visible: flashClicked ? false : true,
-                    child: GestureDetector(
-                      onTap: () {
-                        description = description == 0 ? 1 : 0;
-                        startCamera(description);
-                      },
-                      child: FaIcon(
-                        FontAwesomeIcons.qrcode,
-                        color: Colors.white,
+                    )),
+                if (_focusPoint != null)
+                  Positioned.fill(
+                    top: _focusPoint!.dy * constraints.maxHeight,
+                    child: Align(
+                      alignment: Alignment(
+                          _focusPoint!.dx * 2 - 1, _focusPoint!.dy * 2 - 1),
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.white)),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Positioned(
-                left: 10,
-                right: 10,
-                bottom: 150,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        isPhotoMode = true;
-                        changeState();
-                      },
-                      child: Text(
-                        'Photo',
-                        style: TextStyle(
-                            color: isPhotoMode ? Colors.amber : Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        isPhotoMode = false;
-                        changeState();
-                      },
-                      child: Text(
-                        'Video',
-                        style: TextStyle(
-                            color: isPhotoMode ? Colors.white : Colors.amber,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    ),
-                  ],
-                )),
-            Positioned(
-                left: 10,
-                right: 10,
-                bottom: 40,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        pickImage();
-                      },
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.black.withOpacity(0.2),
+                Positioned(
+                  top: 30,
+                  left: 20,
+                  right: 20,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                          onTap: () async {
+                            flashClicked = !flashClicked;
+                            setState(() {});
+                          },
+                          child: flashClicked
+                              ? flashRow()
+                              : SizedBox(
+                                  height: 50, width: 50, child: flashIcon)),
+                      Visibility(
+                        visible: flashClicked ? false : true,
+                        child: GestureDetector(
+                          onTap: () {
+                            description = description == 0 ? 1 : 0;
+                            startCamera(description);
+                          },
+                          child: FaIcon(
+                            FontAwesomeIcons.qrcode,
+                            color: Colors.white,
+                          ),
                         ),
-                        child: pictureFile != null
-                            ? ClipRRect(
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                    left: 10,
+                    right: 10,
+                    bottom: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            isPhotoMode = true;
+                            changeState();
+                          },
+                          child: Text(
+                            'Photo',
+                            style: TextStyle(
+                                color:
+                                    isPhotoMode ? Colors.amber : Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            isPhotoMode = false;
+                            changeState();
+                          },
+                          child: Text(
+                            'Video',
+                            style: TextStyle(
+                                color:
+                                    isPhotoMode ? Colors.white : Colors.amber,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    )),
+                Positioned(
+                    left: 10,
+                    right: 10,
+                    bottom: 40,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            pickImage();
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.black.withOpacity(0.2),
+                            ),
+                            child: pictureFile != null
+                                ? ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image.file(File(pictureFile!.path),
+                                        fit: BoxFit.fill))
+                                : Container(),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            captureImage();
+                          },
+                          child: Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(width: 5, color: Colors.white),
+                                  shape: BoxShape.circle)),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            toggleCamera();
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(50),
-                                child: Image.file(File(pictureFile!.path),
-                                    fit: BoxFit.fill))
-                            : Container(),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        captureImage();
-                      },
-                      child: Container(
-                          height: 70,
-                          width: 70,
-                          decoration: BoxDecoration(
-                              border: Border.all(width: 5, color: Colors.white),
-                              shape: BoxShape.circle)),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        toggleCamera();
-                      },
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.black.withOpacity(0.2)),
-                        child: Center(
-                          child: FaIcon(FontAwesomeIcons.rotate,
-                              color: Colors.white),
+                                color: Colors.black.withOpacity(0.2)),
+                            child: Center(
+                              child: FaIcon(FontAwesomeIcons.rotate,
+                                  color: Colors.white),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
-                )),
-          ],
+                      ],
+                    )),
+              ],
+            );
+          },
         ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () async {
+        //     pictureFile = await controller.takePicture();
+        //     print(pictureFile?.path.toString());
+        //     setState(() {});
+        //   },
+        //   child: Icon(Icons.camera_alt),
+        // ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () async {
-      //     pictureFile = await controller.takePicture();
-      //     print(pictureFile?.path.toString());
-      //     setState(() {});
-      //   },
-      //   child: Icon(Icons.camera_alt),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
